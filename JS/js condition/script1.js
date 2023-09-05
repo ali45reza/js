e5cdfc2b152c318ee7b3f1a46591301961ce9176
ali45reza <139990270+ali45reza@users.noreply.github.com>
2023-09-05 16:42:42 +0200

    Add files via upload
    des exercises js avec if, else, while et Array

---
+++ JS/js condition/script1.js
@@ -0,0 +1,23 @@
+var nombre_inc = 10
+var nombre_entre;
+nombre_entre = prompt("quel le incnnu?")
+
+if(nombre_entre > nombre_inc){
+    alert("Trop grand");}
+
+
+if(nombre_inc > nombre_entre){
+    alert("Trop petit");}
+
+
+
+else{
+    alert("c'est top");}
+
+// if(nombre_entre == nombre_inc){
+//     alert("c'est top");}
+let n = 0;
+while(n < 5){
+    n++;
+}
+console.log(n);
