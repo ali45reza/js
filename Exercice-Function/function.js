dbfe44f970743a91bf6c95cb480cc9facf1a3416
ali45reza <139990270+ali45reza@users.noreply.github.com>
2023-09-13 11:29:20 +0200

    Add files via upload
    des Exercices sur different function et bocls

---
+++ Exercice-Function/function.js
@@ -0,0 +1,28 @@
+                            // Exercice 1
+// function Salutation(prenom , nom){
+
+//     prenom = prompt("Entrez ton prenom mon ami : ")
+//     nom = prompt("Entrez ton nom mon ami : ")
+//     alert("salut mon ami: "+ prenom +" "+ nom );
+// }
+// Salutation()
+
+                            // Exercice 2
+
+
+// function calcul(a , b){
+
+//     a = parseInt(prompt("Entrez ton 1ére numbre: "));
+//     b = parseInt(prompt("Entrez ton 2éme numbre: "));
+//     return a + b;
+    
+// }
+// console.log(calcul());
+
+                            // Exercice 3
+function pain(){
+ for(i=0; i <= 99; i++){
+    console.log("jaime le pain");
+ }
+}
+pain();
