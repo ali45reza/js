313d9c37166dd30628fb8b1e7f01fb9b0142dca6
ali45reza <139990270+ali45reza@users.noreply.github.com>
2023-09-13 16:13:20 +0200

    Add files via upload
    des function et bocls

---
+++ function-bocls/function-bocls.js
@@ -0,0 +1,9 @@
+let tab = []
+let numbre = parseInt(prompt("Entrez votre chiffre que vous voulez: "))
+function aleatoires(){
+    for(i=0; i <= numbre; i++){
+       tab[i] = Math.random(2)*20
+    }
+   }
+  aleatoires()
+   
